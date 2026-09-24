4ec150eff2a80b42daeb2c9840169ad3c7e01155
aminmasud <aminmasud@outlook.com>
2026-09-24 01:59:51 -0400

    files were edited

--- electron/main.js
+++ electron/main.js
@@ -4,10 +4,16 @@ const path = require("path");
 function createWindow() {
     const window = new BrowserWindow({
         width: 1000,
-        height: 700,
+        height: 700
     });
 
-    window.loadFile(path.join(__dirname, "../renderer/index.html"));
+    if (app.isPackaged) {
+        window.loadFile(
+            path.join(__dirname, "../renderer/dist/index.html")
+        );
+    } else {
+        window.loadURL("http://localhost:5173");
+    }
 }
 
 app.whenReady().then(() => {
--- renderer/main.jsx
+++ renderer/main.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
+function App() {
+    return (
+        <div>
+            <h1>Veyling Studio</h1>
+            <p>React and JSX are working!</p>
+        </div>
+    );
+}
+
 const root = createRoot(document.getElementById("root"));
 
-root.render(
-    <h1>Veyling Studio</h1>
-);
+root.render(<App />);
